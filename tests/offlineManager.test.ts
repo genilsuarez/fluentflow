eda5f7d10c6f35ba8ca88d52c2ef6b94a98adaaf
Genil Alejandro Suarez Perez <gsuarez@transbank.cl>
2026-03-11 20:23:01 -0300

    fix(tests): update offlineManager.test.ts

--- tests/offlineManager.test.ts
+++ tests/offlineManager.test.ts
@@ -150,8 +150,8 @@ describe('offlineManager', () => {
   });
 
   describe('CACHE_NAME', () => {
-    it('should be "fluentflow-offline-v4"', () => {
-      expect(CACHE_NAME).toBe('fluentflow-offline-v4');
+    it('should be "fluentflow-offline-v5"', () => {
+      expect(CACHE_NAME).toBe('fluentflow-offline-v5');
     });
   });
 
